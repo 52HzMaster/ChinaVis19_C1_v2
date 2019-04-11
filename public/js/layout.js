4f150d906995d188bc9c862406e1a4296b877716
Liu Liang <liuliangswust@qq.com>
2019-04-11 11:57:52 +0800

    :shirt:trajectory.

--- public/js/layout.js
+++ public/js/layout.js
@@ -1,3 +1,68 @@
+let floor1_areas = {};
+let floor2_areas = {};
+//分会场
+floor1_areas.area_A = area_compute(2,1,3,5);
+floor1_areas.area_B = area_compute(4,1,5,5);
+floor1_areas.area_C = area_compute(6,1,7,5);
+floor1_areas.area_D = area_compute(8,1,9,5);
+//签到处  海报区
+floor1_areas.area_sign = area_compute(12,2,13,5);
+floor1_areas.area_poster = area_compute(3,7,9,8);
+//厕所
+floor1_areas.area_wc1 = area_compute(4,10,5,11);
+floor1_areas.area_wc2 = area_compute(14,27,15,28);
+//房间1~4
+floor1_areas.area_room1 = area_compute(6,10,9,11);
+floor1_areas.area_room2 = area_compute(10,10,11,11);
+floor1_areas.area_room3 = area_compute(14,21,15,24);
+floor1_areas.area_room4 = area_compute(14,25,15,26);
+//扶梯1~2
+floor1_areas.area_ladder1 = area_compute(1,10,1,11);
+floor1_areas.area_ladder2 = area_compute(14,10,14,11);
+//服务台 展厅 主会场
+floor1_areas.area_serve = area_compute(14,19,15,20);
+floor1_areas.area_disc = area_compute(2,15,11,18);
+floor1_areas.area_main = area_compute(2,19,11,28);
+//进出口
+floor1_areas.area_in = [{x:13,y:0},{x:15,y:2},{x:15,y:4},{x:15,y:7}];
+floor1_areas.area_out = [{x:0,y:19},{x:15,y:5},{x:15,y:15},{x:15,y:17}];
+
+//餐厅
+floor2_areas.area_canteen = area_compute(2,1,9,5);
+//房间5~6
+floor2_areas.area_room5 = area_compute(10,1,11,5);
+floor2_areas.area_room6 = area_compute(6,10,7,11);
+//休闲区
+floor2_areas.area_leisure = area_compute(13,0,15,5);
+//厕所3
+floor2_areas.area_wc3 = area_compute(4,10,5,11);
+//扶梯1~2
+floor2_areas.area_ladder3 = area_compute(1,10,1,11);
+floor2_areas.area_ladder4 = area_compute(14,10,14,11);
+
+function area_compute(x1,y1,x2,y2) {
+    let data= [];
+    for(let i=x1;i<x2+1;i++)
+        for(let j=y1;j<y2+1;j++)
+            data.push({x:i,y:j});
+    return data;
+}
+
+let floor1_area = ["area_A","area_B","area_C","area_D",
+    "area_sign","area_poster",
+    "area_ladder1","area_ladder2",
+    "area_wc1","area_wc2",
+    "area_room1","area_room2","area_room3","area_room4",
+    "area_serve", "area_disc","area_main",
+    "area_in", "area_out"
+];
+
+let floor2_area = ["area_canteen","area_leisure",
+    "area_wc3",
+    "area_ladder3","area_ladder4",
+    "area_room5","area_room6"
+];
+
 let colorScale = {"area_A":"#306bff","area_B":"#34a8ff","area_C":"#44caff","area_D":"#5afaff",
     "area_canteen":"#ffd172",
     "area_leisure":"#c8ff32",
@@ -22,7 +87,7 @@ let all_areas = [
     "area_other"
 ];
 
-let day_url = '/day1_data';
+let day_url = '/day1_pro';
 
 layout();
 
--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,11 @@ let router = express.Router();
 let MongoClient = require('mongodb').MongoClient;
 let DB_CONN_STR = 'mongodb://localhost:27017/chinavis19';
 
-router.get('/day1_data', function(req, res, next) {
+router.get('/day1_pro', function(req, res, next) {
 
     let selectData = function (db, callback) {
         //连接到表
-        let collection = db.collection('day1_data');
+        let collection = db.collection('day1_pro');
         //查询数据
         collection.find({}, {
             "_id": 0
@@ -29,12 +29,12 @@ router.get('/day1_data', function(req, res, next) {
     });
 
 });
-router.get('/day1_data_area', function(req, res, next) {
+router.get('/day1_pro_area', function(req, res, next) {
 
     console.log(req.query.area);
     let selectData = function(db, callback) {
         //连接到表
-        let collection = db.collection('day1_data');
+        let collection = db.collection('day1_pro');
         //查询数据
         collection.find({area:req.query.area},{
             "_id":0
@@ -56,19 +56,22 @@ router.get('/day1_data_area', function(req, res, next) {
     });
 
 });
-router.get('/day1_data_id', function(req, res, next) {
+
+router.get('/day1_pro_id', function(req, res, next) {
 
     let selectData = function(db, callback) {
         //连接到表
-        let collection = db.collection('day1_data');
+        let collection = db.collection('day1_pro');
+        console.log(req.query.id,req.query.floor);
         //查询数据
         //,area:{$ne:"area_other"},
         //             $and:[{area:{$ne:"area_ladder1"}},{area:{$ne:"area_ladder2"}},{area:{$ne:"area_ladder3"}},{area:{$ne:"area_ladder4"}}]}
-        collection.find({id:req.query.id,area:{$ne:"area_other"}}, {
+        collection.find({id:parseInt(req.query.id),floor:parseInt(req.query.floor)}, {
             "_id":0,
-            "stay":0,
-            "date":0,
-            "sid":0
+            'id':0,
+            'sid':0,
+            'floor':0,
+            //"stay":0,
         }).toArray(function(err, result) {
             if(err)
             {
@@ -87,11 +90,11 @@ router.get('/day1_data_id', function(req, res, next) {
     });
 
 });
-router.get('/day1_data_date', function(req, res, next) {
+router.get('/day1_pro_date', function(req, res, next) {
 
     let selectData = function(db, callback) {
         //连接到表
-        let collection = db.collection('day1_data');
+        let collection = db.collection('day1_pro');
         //查询数据
         console.log(req.query.date_start,req.query.date_end);
         collection.find({date:{$gte:req.query.date_start,$lte:req.query.date_end}},{
